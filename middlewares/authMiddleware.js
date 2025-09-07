9a984741c765bde9b7a57a5903d8efb5f253e8a8
mirzaibtisam41 <mirzaibtisam41@gmail.com>
2025-09-07 11:37:38 +0500

    Frontend

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,5 +1,5 @@
 import jwt from 'jsonwebtoken';
-import User from '../models/User.js';
+import User from '../models/user';
 import {StatusCodes} from 'http-status-codes';
 
 export const protect = async (req, res, next) => {
