3a28687d38aecf16f8dabf898648fa9fd67cd08d
mirzaibtisam41 <mirzaibtisam41@gmail.com>
2025-09-07 11:40:04 +0500

    Frontend

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,5 +1,5 @@
 import jwt from 'jsonwebtoken';
-import User from '../models/user';
+import User from '../models/user.js';
 import {StatusCodes} from 'http-status-codes';
 
 export const protect = async (req, res, next) => {
--- server.js
+++ server.js
@@ -27,6 +27,8 @@ app.use(
 app.use(express.json());
 app.use(cookieParser());
 
+app.get('/', (req, res) => res.send('Server is running'));
+
 app.use('/api/auth', authRoutes);
 app.use('/api/categories', categoryRoutes);
 app.use('/api/products', productRoutes);
